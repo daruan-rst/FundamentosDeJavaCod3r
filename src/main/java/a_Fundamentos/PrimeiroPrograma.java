400eae6256a163ce9739177bf1a600397806822c
daruan <daruan.rst@gmail.com>
2022-07-25 22:07:07 -0300

    Initial commit

---
+++ src/main/java/a_Fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package a_Fundamentos;
+
+/**
+ * Essa classe representa ... JAVADOC
+ *
+ * @author daruan <daruan.rst@gmail.com>
+ * @since JDK 1.0
+ * @see www.udemy.com/share/101rUm3@tTdcwokCQfBcXj7TrSE_gh7Tdjzu-f7xFW66qKUI_ex1iXPBXtKa0i5hpbCkBi0K/
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        //Uma sentença de código termina com ;
+        System.out.println("Primeiro programa Parte #01!");
+
+        /*
+         * LINHA 1
+         * LINHA 2
+         * LINHA 3
+         */
+        System.out.println("Primeiro programa Parte #02!");
+    }
+}

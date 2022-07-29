5f6aabeb7d1c0ef1083d5994d11b00d7d00a9183
daruan <daruan.rst@gmail.com>
2022-07-28 22:36:43 -0300

    String e Imports

---
+++ src/main/java/a_Fundamentos/TipoString.java
@@ -0,0 +1,39 @@
+package a_Fundamentos;
+
+public class TipoString {
+    public static void main(String[] args) {
+        System.out.println("Olá todo mundo".charAt(2));
+
+        String s = "Boa Tarde";
+        System.out.println(s.concat("!!!"));
+        //o mesmo resultado que
+        System.out.println(s+"!!!");
+        System.out.println(s.startsWith("Boa"));
+        System.out.println(s.toLowerCase().startsWith("boa"));
+        System.out.println(s.toUpperCase().startsWith("TARDE"));
+        System.out.println(s.length());
+        System.out.println(s.toLowerCase().equals("boa tarde"));
+        System.out.println(s.equalsIgnoreCase("boa tarde"));
+
+        var nome = "Pedro";
+        var sobrenome = "Santos";
+        var idade = 33;
+        var salario = 12345.987;
+
+        System.out.println("Nome: " + nome + "\nSobrenome: " + sobrenome
+                            +"\nIdade: " + idade + "\nSalario: " + salario);
+
+        String outraFrase = "Nome: " + nome + "\nSobrenome: " + sobrenome
+                +"\nIdade: " + idade + "\nSalario: " + salario;
+        System.out.println(outraFrase);
+
+        System.out.printf("O senhor %s $s tem $d anos e ganha R$%.2f.", nome, sobrenome, idade, salario);
+        String frase = String.format("\n\"O senhor %s $s tem $d anos e ganha R$%.2f.\", nome, sobrenome, idade, salario");
+        System.out.println(frase);
+
+        System.out.println("Frase qualquer".contains("qual"));
+        System.out.println("Frase qualquer".indexOf("qual"));
+        System.out.println("Frase qualquer".substring(6));
+        System.out.println("Frase qualquer".substring(6,10));
+    }
+}

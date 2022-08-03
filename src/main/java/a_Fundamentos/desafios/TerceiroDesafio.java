14705e53a62ef61656ab90d95e6460d619820c11
daruan <daruan.rst@gmail.com>
2022-08-02 23:45:25 -0300

    Operadores aritméticos

---
+++ src/main/java/a_Fundamentos/desafios/TerceiroDesafio.java
@@ -0,0 +1,12 @@
+package a_Fundamentos.desafios;
+
+public class TerceiroDesafio {
+    public static void main(String[] args) {
+
+        double a = (Math.pow((6*(3+2)), 2))/6;
+        double b = Math.pow(((1-5)*(2-7)/2.0),2);
+        double result = Math.pow((a-b)/10.0, 3);
+        System.out.println(result);
+
+    }
+}

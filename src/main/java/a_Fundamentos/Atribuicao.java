6577580168282e0648b8a34a425d6fa2f0bb93b2
daruan <daruan.rst@gmail.com>
2022-08-05 19:22:26 -0300

    Operadores de Atribuição

---
+++ src/main/java/a_Fundamentos/Atribuicao.java
@@ -0,0 +1,19 @@
+package a_Fundamentos;
+
+public class Atribuicao {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = a;
+        int c = a+b;
+
+        c += b; // é o mesmo que c = c + b;
+        c -= a; // é o mesmo que c = c - a;
+        c *= b; // é o mesmo que c = c * b;
+        c /= a; // é o mesmo que c = c / a;
+        c %= 2; // é o mesmo que c = c % 2;
+
+        System.out.println(c);
+    }
+
+}

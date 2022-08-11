5c7f270968f7b915c5f0258f9d001595ea27f3fa
daruan <daruan.rst@gmail.com>
2022-08-11 18:21:51 -0300

    Estruturas de Controle: If Else If

---
+++ src/main/java/b_EstruturasDeControle/IfElse.java
@@ -0,0 +1,21 @@
+package b_EstruturasDeControle;
+
+import javax.swing.*;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog("Informe o número:");
+        int numero = Integer.parseInt(valor);
+
+        if (numero%2 == 0){
+            System.out.println("Número par");
+        }else {
+            System.out.println("Numero ímpar");
+//            É o mesmo que
+//            if (numero%2 == 1){
+//                System.out.println("Número impar");
+//            }
+        }
+    }
+}

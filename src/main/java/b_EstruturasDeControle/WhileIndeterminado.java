6c8c54580e2947a023fc20ab25f66f622abbf1a6
daruan <daruan.rst@gmail.com>
2022-08-12 18:46:54 -0300

    Laços de repetição: While e For

---
+++ src/main/java/b_EstruturasDeControle/WhileIndeterminado.java
@@ -0,0 +1,16 @@
+package b_EstruturasDeControle;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String valor = "";
+
+        while (!valor.equalsIgnoreCase("sair")){
+            System.out.println("Voce diz:");
+            valor = sc.nextLine();
+        }
+    }
+}

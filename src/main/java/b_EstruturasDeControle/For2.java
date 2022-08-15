ee4fa888403175f5654e6fca96277315a5c36edb
daruan <daruan.rst@gmail.com>
2022-08-15 20:15:54 -0300

    Laços de repetição: For

---
+++ src/main/java/b_EstruturasDeControle/For2.java
@@ -0,0 +1,10 @@
+package b_EstruturasDeControle;
+
+public class For2 {
+    public static void main(String[] args) {
+
+        for (int i = 10; i>=0 ; i-=2){
+            System.out.println(i);
+        }
+    }
+}

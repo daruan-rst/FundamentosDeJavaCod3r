e45a44ef836cacbfa456636a5207fb180d41f160
daruan <daruan.rst@gmail.com>
2022-08-17 23:51:12 -0300

    Estruturas de Controle: switch

---
+++ src/main/java/b_EstruturasDeControle/SwitchSemBreak.java
@@ -0,0 +1,46 @@
+package b_EstruturasDeControle;
+
+public class SwitchSemBreak {
+    public static void main(String[] args) {
+//        if (bool) ...
+
+        String faixa = "preta";
+//        String faixa = "branca";
+//        String faixa = "amarela";
+
+
+        switch (faixa.toLowerCase()){
+            case "preta":
+                System.out.println("Sei o Bassai-Dai...");
+            case "marrom":
+                System.out.println("Sei o Tekki Shodan");
+            case "roxa":
+                System.out.println("Sei o Heian Godan");
+            case "verde":
+                System.out.println("Sei o Hrian Yodan");
+            case "laranja":
+                System.out.println("Sei o Heian Sandan");
+            case "vermelha":
+                System.out.println("Sei o Heian Nidan");
+            case "amarela":
+                System.out.println("Sei o Heian Shodan");
+                break;
+            default:
+                System.out.println("Não sei de nada");
+        }
+
+        int idade = 2;
+
+        switch(idade){
+            case 3:
+                System.out.println("Sabe programar");
+            case 2:
+                System.out.println("Sabe falar");
+            case 1:
+                System.out.println("Sabe andar");
+            case 0:
+                System.out.println("Sabe respirar");
+        }
+
+    }
+}

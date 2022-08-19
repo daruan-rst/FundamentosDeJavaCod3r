37e48b76b7edac2867e2b42de8b87a277dec629f
daruan <daruan.rst@gmail.com>
2022-08-19 02:01:16 -0300

    Exercicios do módulo de estruturas de controle

---
+++ src/main/java/b_EstruturasDeControle/Exercicios/Exercicio2.java
@@ -0,0 +1,31 @@
+package b_EstruturasDeControle.Exercicios;
+
+import java.util.Scanner;
+
+/**
+ * Criar um programa informa se o ano atual é um ano bissexto;
+ */
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o ano");
+        int ano = sc.nextInt();
+
+        boolean bissexto;
+
+        if (ano % 100 == 0 && ano % 400 == 0){
+            bissexto = true;
+        } else if (ano % 4 == 0) {
+            bissexto = true;
+        }else {
+            bissexto = false;
+        }
+
+        System.out.printf("\nO ano informado %s bissexto" , (bissexto? "é" : "não é"));
+
+        sc.close();
+
+    }
+}
---
+++ src/main/java/b_EstruturasDeControle/Exercicios/Exercicio5.java
@@ -0,0 +1,34 @@
+package b_EstruturasDeControle.Exercicios;
+
+import java.util.Scanner;
+
+/**
+ * Criar um programa que receba um número e diga se ele é um número primo.
+ * TEM QUE USAR SWITCH
+ */
+public class Exercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+        int divisores = 0;
+
+        // O laço de for tem que começar pelo 2, já que qualquer número módulo 1 resulta zero
+        for (int i = 2 ; i<numero ; i++){
+            if (numero%i == 0){
+                divisores++;
+            }
+        }
+
+        switch (divisores){
+            case 0:
+                System.out.println("O número é primo");
+                break;
+            default:
+                System.out.println("O número não é primo");
+                break;
+        }
+
+    }
+}
+

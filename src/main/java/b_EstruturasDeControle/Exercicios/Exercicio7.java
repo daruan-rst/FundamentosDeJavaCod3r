5cd5cb8dd1e460e6c7b425d6036d8b2cbfac0a9c
daruan <daruan.rst@gmail.com>
2022-08-21 20:55:18 -0300

    Exercicios do módulo de estruturas de controle

---
+++ src/main/java/b_EstruturasDeControle/Exercicios/Exercicio7.java
@@ -0,0 +1,26 @@
+package b_EstruturasDeControle.Exercicios;
+
+import java.util.Scanner;
+
+/**
+ * Criar um programa que enquanto estiver recebendo números positivos,
+ * imprime no console a soma dos números inseridos, caso receba um número negativo,
+ * encerre o programa. Tente utilizar a estrutura do while.
+ */
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int soma = 0;
+        int numero = sc.nextInt();
+
+        while (numero>=0){
+            soma+=numero;
+            System.out.println("Soma atual é " +  soma +"\nDigite o próximo número:");
+            numero = sc.nextInt();
+        }
+
+
+        sc.close();
+
+    }
+}

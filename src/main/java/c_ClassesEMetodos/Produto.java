6649d37f9fda86c0254d43012f273a3da7664d76
daruan <daruan.rst@gmail.com>
2022-08-27 23:41:56 -0300

    Classes e métodos: construtores

--- src/main/java/c_ClassesEMetodos/Produto.java
+++ src/main/java/c_ClassesEMetodos/Produto.java
@@ -5,6 +5,18 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
+
+    //Construtor padrão i.e. sem parâmetros
+    Produto(){
+
+    }
+
+    Produto(String nomeInicial, double precoInicial, double descontoInicial){
+        this.nome = nomeInicial;
+        this.preco = precoInicial;
+        this.desconto = descontoInicial;
+    }
+
     double precoComDesconto(){
         return preco * (1 - desconto);
     }
--- src/main/java/c_ClassesEMetodos/ProdutoTeste.java
+++ src/main/java/c_ClassesEMetodos/ProdutoTeste.java
@@ -3,12 +3,12 @@ package c_ClassesEMetodos;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto();
-        p1.nome = "Notebook";
-        p1.preco = 4356.89;
-        p1.desconto = 0.25;
+        Produto p1 = new Produto("Notebook", 4356.89, 0.25);  // construtor usado: com todos os parâmetros
+//        p1.nome = "Notebook";
+//        p1.preco = 4356.89;
+//        p1.desconto = 0.25;
 
-        var p2 = new Produto();
+        var p2 = new Produto(); // construtor usado: sem parâmetros
         p2.nome = "Caneta Preta";
         p2.preco = 12.56;
         p2.desconto = 0.29;

08e76824f2a7594baed9976b7cc57d30d734235d
daruan <daruan.rst@gmail.com>
2022-09-01 18:19:41 -0300

    Classes e métodos: desafio "primeiro trauma"

---
+++ src/main/java/c_ClassesEMetodos/Desafios/PrimeiroTrauma.java
@@ -0,0 +1,22 @@
+package c_ClassesEMetodos.Desafios;
+
+/**
+ * Dada a classe PrimeiroTrauma e a variável "a",
+ * como alterar o código de modo que a seja acessível dentro da função main?
+ *
+ public class PrimeiroTrauma{
+
+    int a = 3; // Não pode mexer nessa linha
+
+    public static void main(String[] args){
+    System.out.println(a);
+    }
+ }
+ */
+public class PrimeiroTrauma{
+    int a = 3; // Não pode mexer nessa linha
+    public static void main(String[] args){
+        PrimeiroTrauma pt = new PrimeiroTrauma();
+        System.out.println(pt.a);
+    }
+}

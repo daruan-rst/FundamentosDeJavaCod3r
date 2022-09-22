1914bdefa486f4ebbf3b4b24818a208ee13890dc
daruan <daruan.rst@gmail.com>
2022-09-21 22:00:02 -0300

    Orientação a Objetos: Relacionamento um para um

---
+++ src/main/java/e_OrientacaoAObjetos/UmParaUm/Motor.java
@@ -0,0 +1,18 @@
+package e_OrientacaoAObjetos.UmParaUm;
+
+public class Motor{
+
+    final Carro carro;
+    boolean ligado = false;
+    double fatorInjecao = 1;
+    public Motor(Carro carro){
+        this.carro = carro;
+    }
+    int giros(){
+        if(!ligado){
+            return 0;
+        }else{
+            return (int) Math.round(fatorInjecao * 3000);
+        }
+    }
+}

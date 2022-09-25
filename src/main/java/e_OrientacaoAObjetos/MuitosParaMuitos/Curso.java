129edca0f380a5be1c138b3c5a062d43701f0785
daruan <daruan.rst@gmail.com>
2022-09-24 21:01:22 -0300

    Orientação a Objetos: Relacionamento muitos para muitos

---
+++ src/main/java/e_OrientacaoAObjetos/MuitosParaMuitos/Curso.java
@@ -0,0 +1,21 @@
+package e_OrientacaoAObjetos.MuitosParaMuitos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curso {
+
+    final String nome;
+    final List<Aluno> alunos = new ArrayList<>();
+
+    Curso(String nome){
+        this.nome = nome;
+    }
+
+    void adicionarAluno(Aluno aluno){
+        alunos.add(aluno);
+        aluno.cursos.add(this);
+//        aluno.adicionarCurso(this);
+
+    }
+}

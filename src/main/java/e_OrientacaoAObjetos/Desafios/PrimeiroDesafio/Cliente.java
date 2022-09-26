edc8124c9074ae2b23dfcc1dc44babe4a6c3a418
daruan <daruan.rst@gmail.com>
2022-09-26 19:29:31 -0300

    Orientação a Objetos: Relacionamento muitos para muitos

--- src/main/java/e_OrientacaoAObjetos/Desafios/PrimeiroDesafio/Cliente.java
+++ src/main/java/e_OrientacaoAObjetos/Desafios/PrimeiroDesafio/Cliente.java
@@ -1,7 +1,6 @@
 package e_OrientacaoAObjetos.Desafios.PrimeiroDesafio;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/e_OrientacaoAObjetos/Desafios/PrimeiroDesafio/Compra.java
+++ src/main/java/e_OrientacaoAObjetos/Desafios/PrimeiroDesafio/Compra.java
@@ -1,5 +1,6 @@
 package e_OrientacaoAObjetos.Desafios.PrimeiroDesafio;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,4 +10,22 @@ public class Compra {
     void adicionarItem(Item item){
         this.itens.add(item);
     }
+
+    //Métodos novos da correção
+
+    void adicionarItem(Produto p, int qtde){
+        this.adicionarItem(new Item(qtde, p));
+    }
+
+    void adicionarItem(String nome, BigDecimal preco, int qtde){
+        this.adicionarItem(new Produto(nome, preco), qtde);
+    }
+
+    BigDecimal obterValorTotal(){
+        BigDecimal total = BigDecimal.ZERO;
+        for (Item item: itens
+             ) {
+            total = total.add(item.produto.preco.multiply(new BigDecimal(item.quantidade)));
+        }
+    return total;}
 }

152c897308bad34484f0a861e5eb753d5b3e18bb
daruan <daruan.rst@gmail.com>
2022-09-25 23:12:38 -0300

    Orientação a Objetos: Desafio composição

---
+++ src/main/java/e_OrientacaoAObjetos/Desafios/PrimeiroDesafio/Compra.java
@@ -0,0 +1,12 @@
+package e_OrientacaoAObjetos.Desafios.PrimeiroDesafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Compra {
+    List<Item> itens = new ArrayList<>();
+
+    void adicionarItem(Item item){
+        this.itens.add(item);
+    }
+}
---
+++ src/main/java/e_OrientacaoAObjetos/Desafios/PrimeiroDesafio/Item.java
@@ -0,0 +1,11 @@
+package e_OrientacaoAObjetos.Desafios.PrimeiroDesafio;
+
+public class Item {
+    int quantidade;
+    Produto produto;
+
+    Item(int qntd, Produto produto){
+        this.quantidade = qntd;
+        this.produto = produto;
+    }
+}

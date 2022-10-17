269a557977300345040c6f887e21ce7d56601cc3
daruan <daruan.rst@gmail.com>
2022-10-17 20:49:13 -0300

    Orientação a Objetos:  Herança #03 - Estudo de herança e construtores

--- src/main/java/e_OrientacaoAObjetos/Herança/Heroi.java
+++ src/main/java/e_OrientacaoAObjetos/Herança/Heroi.java
@@ -2,6 +2,10 @@ package e_OrientacaoAObjetos.Herança;
 
 public class Heroi extends Jogador {
 
+    Heroi(int x, int y){
+        super(x,y);
+    }
+
     boolean atacar(Jogador oponente) {
         boolean atacar1 = super.atacar(oponente);
         boolean atacar2 = super.atacar(oponente);

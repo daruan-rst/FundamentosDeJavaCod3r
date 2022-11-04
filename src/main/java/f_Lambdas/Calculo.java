c7b58cc90517634e53ceac14292abfc7062e2e8c
daruan <daruan.rst@gmail.com>
2022-11-03 21:33:27 -0300

    Lambda: Interface Funcional

--- src/main/java/f_Lambdas/Calculo.java
+++ src/main/java/f_Lambdas/Calculo.java
@@ -1,6 +1,17 @@
 package f_Lambdas;
 
+@FunctionalInterface //-> Lambdas só podem ser definidas sob interfaces funcionais
 public interface Calculo {
 
     public abstract double executar(double a, double b);
+
+//    double teste(); -> anotação @FunctionalInterface impede que mais de um método ABSTRATO seja criado numa interface
+
+    default String legal(){ // -> métodos default não inviabilizam interfaces funcionais
+        return "legal";
+    }
+
+    static String muitoLegal(){ // -> métodos estáticos não inviabilizam interfaces funcionais
+        return "muito legal";
+    }
 }

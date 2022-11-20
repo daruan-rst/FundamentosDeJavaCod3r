e4410d51e897e293ef952c03492f14d9938716f7
daruan <daruan.rst@gmail.com>
2022-11-19 21:33:39 -0300

    Lambda: Interfaces Funcionais antes do Java 8

---
+++ src/main/java/f_Lambdas/Trabalho1.java
@@ -0,0 +1,13 @@
+package f_Lambdas;
+
+public class Trabalho1 implements Runnable{
+
+
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Trabalho #01");
+            }
+        }
+
+
+}

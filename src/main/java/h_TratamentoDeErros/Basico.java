68f0b44cbcca9bd37ef5ed535c6bc8359915cd42
daruan-rst <daruan.rst@gmail.com>
2022-12-16 18:42:14 -0300

    Tratamento de erros: O Básico sobre exceções

---
+++ src/main/java/h_TratamentoDeErros/Basico.java
@@ -0,0 +1,33 @@
+package h_TratamentoDeErros;
+
+
+import g_Streams.Aluno;
+
+public class Basico {
+
+    public static void main(String[] args) {
+
+        Aluno a1 = null;
+
+        try{
+            imprimirNomeDoAluno(a1); // NullPointerException
+        }catch (Exception excecao){
+            System.out.println("Ocorreu um erro no momento " +
+                    "de imprimir o nome do usuário");
+        }
+
+
+
+        try {
+            System.out.println(7 / 0);
+        }catch (ArithmeticException e){
+            System.out.println("Ococrreu o erro: " +  e.getMessage());
+        }
+        
+        System.out.println("Fim");
+    }
+
+    public static void imprimirNomeDoAluno(Aluno aluno){
+        System.out.println(aluno.toString());
+    }
+}

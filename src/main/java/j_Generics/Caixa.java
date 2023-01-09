51448bc12b9337d8dfff13644f6a12670ab26ead
daruan-rst <daruan.rst@gmail.com>
2023-01-09 12:34:37 -0300

    Generics: Exemplo Básico COM Generics

---
+++ src/main/java/j_Generics/Caixa.java
@@ -0,0 +1,14 @@
+package j_Generics;
+
+public class Caixa<T> {
+
+    private T coisa;
+
+    public void guardar(T coisa){
+        this.coisa = coisa;
+    }
+
+    public T abrir(){
+        return coisa;
+    }
+}

ee27b34df92795087b34390f8902b6e4d80f9707
daruan-rst <daruan.rst@gmail.com>
2023-01-25 15:09:43 -0300

    Java Desktop com Swing: Componente Botão

--- src/main/java/k_JavaSwing/calculadora/visao/Teclado.java
+++ src/main/java/k_JavaSwing/calculadora/visao/Teclado.java
@@ -1,8 +1,13 @@
 package k_JavaSwing.calculadora.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Teclado extends JPanel {
+
+    private final Color COR_CINZA_ESCURO = new Color(68,69,68);
+    private final Color COR_CINZA_CLARO = new Color(97,100,99);
+    private final Color COR_LARANJA = new Color(242,163,60);
     public Teclado() {
 
     }

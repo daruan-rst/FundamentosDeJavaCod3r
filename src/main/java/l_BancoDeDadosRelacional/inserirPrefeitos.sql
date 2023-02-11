e60f98da6960299f285e3bf08f4f2899eb1eb0b4
daruan-rst <daruan.rst@gmail.com>
2023-02-11 09:33:00 -0300

    Banco de Dados Relacional: Inserir Prefeitos

---
+++ src/main/java/l_BancoDeDadosRelacional/inserirPrefeitos.sql
@@ -0,0 +1,17 @@
+INSERT INTO prefeitos
+	(nome, cidade_id)
+VALUES
+	('Rodrigo Neves', 2),
+	('Raquel Lyra', 3),
+	('Zenaldo Coutinho', null);
+	
+INSERT INTO prefeitos 
+		(nome, cidade_id)
+VALUES
+		('Rafael Greca', null)
+		
+/* Duplicate entry '3' for key 'prefeitos.cidade_id'
+INSERT INTO prefeitos 
+		(nome, cidade_id)
+VALUES
+		('Rodrigo Pinheiro', 3); */

1fb4edf9a5596ec87aff8d5927745fec9759abeb
daruan-rst <daruan.rst@gmail.com>
2023-02-15 20:24:49 -0300

    Banco de Dados com JDBC: Abrindo uma Conexão

---
+++ src/main/java/m_BancoDeDadosComJDBC/TesteConexao.java
@@ -0,0 +1,23 @@
+package m_BancoDeDadosComJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+
+    public static void main(String[] args) throws SQLException {
+
+       final String stringDeConexao = "jdbc:mysql://localhost:3307?verifyServerCertificate=false&useSSL=true";
+
+       final String usuario = "root";
+
+       final String senha = "root";
+
+        Connection conexao = DriverManager
+                .getConnection(stringDeConexao, usuario, senha);
+
+        System.out.println("Conexão efetuada com sucesso!");
+        conexao.close();
+    }
+}

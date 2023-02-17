baa868487d0f486c4a6b2c82430956df617b5fbf
daruan-rst <daruan.rst@gmail.com>
2023-02-17 20:26:35 -0300

    Banco de Dados com JDBC:Fábrica de Conexões

---
+++ src/main/java/m_BancoDeDadosComJDBC/FabricaConexao.java
@@ -0,0 +1,20 @@
+package m_BancoDeDadosComJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+    public static Connection getConexao(){
+        try {
+            final String stringDeConexao = "jdbc:mysql://localhost:3307?verifyServerCertificate=false&useSSL=true";
+            final String usuario = "root";
+            final String senha = "root";
+
+            return DriverManager.getConnection(stringDeConexao, usuario, senha);
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}

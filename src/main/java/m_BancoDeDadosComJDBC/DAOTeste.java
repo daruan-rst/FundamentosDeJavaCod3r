8d6dd7eebae5417fec2e7810c494eb4e9f98e709
daruan-rst <daruan.rst@gmail.com>
2023-02-28 23:52:56 -0300

    Banco de Dados com JDBC: Padrão DAO

---
+++ src/main/java/m_BancoDeDadosComJDBC/DAOTeste.java
@@ -0,0 +1,18 @@
+package m_BancoDeDadosComJDBC;
+
+public class DAOTeste {
+
+    public static void main(String[] args) {
+
+        DAO dao = new DAO();
+
+        String sql  = "INSERT INTO pessoas (nome) VALUES (?, ?)";
+
+        System.out.println(dao.incluir(sql, "João da Silva", 1000));
+        System.out.println(dao.incluir(sql, "Ana Julia", 1001));
+        System.out.println(dao.incluir(sql, "Djalma Pereira", 1002));
+
+        dao.close();
+
+    }
+}

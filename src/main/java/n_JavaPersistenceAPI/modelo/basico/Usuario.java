11694af277b3a1fdc6009b0afcad6e17402cf0fa
daruan-rst <daruan.rst@gmail.com>
2023-03-03 13:31:52 -0300

    Java Persistence API (JPA): Entidade Usuario

---
+++ src/main/java/n_JavaPersistenceAPI/modelo/basico/Usuario.java
@@ -0,0 +1,47 @@
+package n_JavaPersistenceAPI.modelo.basico;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
+@Entity
+public class Usuario {
+
+    @Id
+    private long id;
+
+    private String nome;
+
+    @Transient
+    private String email;
+
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

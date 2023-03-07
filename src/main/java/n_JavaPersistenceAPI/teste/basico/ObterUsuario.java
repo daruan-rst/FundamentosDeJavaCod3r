d79b91665514d34d6b3fdaa04ece767e13566514
daruan-rst <daruan.rst@gmail.com>
2023-03-06 23:46:31 -0300

    Java Persistence API (JPA): Obter Usuário

--- src/main/java/n_JavaPersistenceAPI/teste/basico/ObterUsuario.java
+++ src/main/java/n_JavaPersistenceAPI/teste/basico/ObterUsuario.java
@@ -5,25 +5,17 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import n_JavaPersistenceAPI.modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class ObterUsuario {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence
                 .createEntityManagerFactory("exercicios-jpa");
         EntityManager em = emf.createEntityManager();
 
-        Usuario novoUsuario = new Usuario("Djalma", "djalma@lanche.com.br");
-
-        em.getTransaction().begin();
-        em.persist(novoUsuario);
-        em.getTransaction().commit();
-
-        System.out.println("O Id gerado foi: " + novoUsuario.getId());
+        Usuario usuario = em.find(Usuario.class, 1L);
+        System.out.println(usuario.getNome());
 
         em.close();
         emf.close();
     }
-
 }
-
-

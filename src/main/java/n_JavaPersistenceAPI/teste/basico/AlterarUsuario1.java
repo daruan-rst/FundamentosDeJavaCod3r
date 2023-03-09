ac416f1d5725777ee2dd99e04efd005f83f2af22
daruan-rst <daruan.rst@gmail.com>
2023-03-08 23:42:19 -0300

    Java Persistence API (JPA): Alterar Usuário

---
+++ src/main/java/n_JavaPersistenceAPI/teste/basico/AlterarUsuario1.java
@@ -0,0 +1,27 @@
+package n_JavaPersistenceAPI.teste.basico;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import n_JavaPersistenceAPI.modelo.basico.Usuario;
+
+public class AlterarUsuario1 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Usuario usuario = em.find(Usuario.class, 7L);
+        usuario.setNome("Leonardo");
+        usuario.setEmail("leonardo@lanche.com.br");
+
+        em.merge(usuario);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+}

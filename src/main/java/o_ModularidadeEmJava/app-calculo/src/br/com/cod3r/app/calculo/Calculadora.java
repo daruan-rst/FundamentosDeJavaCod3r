1c9f5e21ffd1ba751e716595b6998ed493f123d7
daruan-rst <daruan.rst@gmail.com>
2023-04-15 23:58:35 -0300

    Modularidade em Java: Requires/Exports

---
+++ src/main/java/o_ModularidadeEmJava/app-calculo/src/br/com/cod3r/app/calculo/Calculadora.java
@@ -0,0 +1,13 @@
+package o_ModularidadeEmJava.app;
+
+import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
+
+public class Calculadora {
+
+    private OperacoesAritmeticas opAritmeticas = new OperacoesAritmeticas();
+
+    public double soma(double... nums) {
+        return opAritmeticas.soma(nums);
+    }
+
+}

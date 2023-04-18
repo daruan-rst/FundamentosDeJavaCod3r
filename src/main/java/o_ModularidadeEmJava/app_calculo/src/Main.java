3181f49fec89b677e8b8cdacf921aa49c779a4ad
daruan-rst <daruan.rst@gmail.com>
2023-04-17 22:53:29 -0300

    Modularidade em Java: Requires/Exports

--- src/main/java/o_ModularidadeEmJava/app_calculo/src/Main.java
+++
@@ -1,7 +0,0 @@
-package o_ModularidadeEmJava.src.app_calculo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/o_ModularidadeEmJava/app_financeiro/src/Main.java
+++
@@ -1,7 +0,0 @@
-package o_ModularidadeEmJava.app;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}

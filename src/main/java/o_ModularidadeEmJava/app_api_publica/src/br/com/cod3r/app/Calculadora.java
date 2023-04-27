176fb94cb55506de8c56dadcd424e0ecc644f186
daruan-rst <daruan.rst@gmail.com>
2023-04-26 23:02:36 -0300

    Modularidade em Java

--- src/main/java/o_ModularidadeEmJava/app_api_publica/src/br/com/cod3r/app/Calculadora.java
+++
@@ -1,5 +0,0 @@
-package o_ModularidadeEmJava.app_api_publica.src.br.com.cod3r.app;
-
-public interface Calculadora {
-    public double soma(double... nums);
-}
--- src/main/java/o_ModularidadeEmJava/app_financeiro/src/financeiro/Calc.java
+++
@@ -1,6 +0,0 @@
-package o_ModularidadeEmJava.app_financeiro.src.financeiro;
-
-public interface Calc {
-
-    public double soma(double... nums);
-}

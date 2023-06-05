d4b15faea7294c5a7d7a45950a28d2642eabcfde
daruan-rst <daruan.rst@gmail.com>
2023-06-04 23:48:58 -0300

    JavaFX: Contador

--- src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/basico/Contador.java
+++ src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/basico/Contador.java
@@ -17,7 +17,10 @@ public class Contador extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Label labelTitulo = new Label("Contador");
+        labelTitulo.getStyleClass().add("titulo");
+
         Label labelNumero  = new Label("0");
+        labelNumero.getStyleClass().add("numero");
 
         Button botaoIncremento = new Button("+");
         botaoIncremento.setOnAction(e -> {
@@ -36,14 +39,19 @@ public class Contador extends Application {
         boxBotoes.getChildren().add(botaoIncremento);
         boxBotoes.getChildren().add(botaoDecremento);
 
-        VBox boxPrincipal = new VBox();
-        boxPrincipal.setAlignment(Pos.CENTER);
+        VBox boxConteudo = new VBox();
+        boxConteudo.getStyleClass().add("conteudo");
+        boxConteudo.setAlignment(Pos.CENTER);
         boxBotoes.setSpacing(10);
-        boxPrincipal.getChildren().add(labelTitulo);
-        boxPrincipal.getChildren().add(labelNumero);
-        boxPrincipal.getChildren().add(boxBotoes);
+        boxConteudo.getChildren().add(labelTitulo);
+        boxConteudo.getChildren().add(labelNumero);
+        boxConteudo.getChildren().add(boxBotoes);
 
-        Scene cenaPrincipal = new Scene(boxPrincipal, 400, 400);
+        String caminhoDoCss = getClass()
+                .getResource("/basico/Contador.css").toExternalForm();
+        Scene cenaPrincipal = new Scene(boxConteudo, 400, 400);
+        cenaPrincipal.getStylesheets().add(caminhoDoCss);
+        cenaPrincipal.getStylesheets().add("https://fonts.googleapis.com/css?family=Oswald");
 
         primaryStage.setScene(cenaPrincipal);
         primaryStage.show();

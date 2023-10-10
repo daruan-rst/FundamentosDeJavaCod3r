655941b596b872cced189d9c2f8080f243dff898
daruan-rst <daruan.rst@gmail.com>
2023-10-09 22:47:18 -0300

    JavaFX: Gerenciadores de Layout

---
+++ src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/layout/AppLayout.java
@@ -0,0 +1,19 @@
+package br.com.cod3r.exerciciosjavafx.layout;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppLayout extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Gerenciadores de Layout");
+        stage.show();
+        //-cp exercicios-javafx -XstartOnFirstThread
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

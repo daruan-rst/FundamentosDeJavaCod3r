4cca45e9f4c6c6f8760e356311659c6ac95d58cb
daruan-rst <daruan.rst@gmail.com>
2023-10-11 23:42:40 -0300

    JavaFX: AnchorPane

--- src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/layout/AppLayout.java
+++ src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/layout/AppLayout.java
@@ -1,15 +1,19 @@
 package br.com.cod3r.exerciciosjavafx.layout;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        Scene principal = new Scene( new TesteAnchorPane(), 800, 600);
+
+        stage.setScene(principal);
         stage.setTitle("Gerenciadores de Layout");
         stage.show();
-        //-cp exercicios-javafx -XstartOnFirstThread
+
 
     }
 

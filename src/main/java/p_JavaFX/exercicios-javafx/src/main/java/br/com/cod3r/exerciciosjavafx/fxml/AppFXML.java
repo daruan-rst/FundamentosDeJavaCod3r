098912ba8f2e0084df9273c28412cf15d4345698
daruan-rst <daruan.rst@gmail.com>
2023-10-17 11:12:32 -0300

    JavaFX: FXML Config

---
+++ src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/fxml/AppFXML.java
@@ -0,0 +1,15 @@
+package br.com.cod3r.exerciciosjavafx.fxml;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFXML extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

14a0da82e973da399f9ee67e93f02ea2a50d1411
daruan-rst <daruan.rst@gmail.com>
2023-10-24 22:47:42 -0300

    JavaFX: Usando CSS com FXML

--- src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/fxml/AppFXML.java
+++ src/main/java/p_JavaFX/exercicios-javafx/src/main/java/br/com/cod3r/exerciciosjavafx/fxml/AppFXML.java
@@ -11,10 +11,12 @@ import java.net.URL;
 public class AppFXML extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        String arquivoCSS = getClass().getResource("/fxml/Login.css").toExternalForm();
         URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
         GridPane raiz = FXMLLoader.load(arquivoFXML);
 
         Scene cena = new Scene(raiz,350,400);
+        cena.getStylesheets().add(arquivoCSS);
         stage.setResizable(false);
         stage.setTitle("Tela de Login");
         stage.setScene(cena);
